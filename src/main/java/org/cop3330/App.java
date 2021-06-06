619e1af66f39c211b9dc73046ef240ca20ca5ae2
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 01:43:08 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,13 @@
+package org.cop3330;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is your age? ");
+        int age = in.nextInt();
+        System.out.println(age >= 16 ? "You are old enough to legally drive." : "You are not old enough to legally drive.");
+    }
+}
